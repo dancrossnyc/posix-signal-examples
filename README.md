7c8884039a476e4c6e1947b49b6bdb323e25dcd7
Dan Cross <cross@gajendra.net>
2025-01-09 21:24:08 +0000

    syncsigjmp and syncsigwait
    syncsigjmp demonstrates using `sigsetmp` and `siglongjmp` to jump
    out of a signal handler and into a dedicated signal-handling thread,
    where the signal mask is set such that we can safely invoke
    `pthread_cond_broadcast` on a condition variable.
    
    Note that we vector all signal delivery to this thread, so that we
    know that we never interrupt a non-async-signal-safe function.
    
    syncsigwait demonstrates vector all signals to a dedicated thread
    that sits in a tight loop running `sigwait`, thus turning
    asynchronous signal delivery a synchronous return from a blocking
    call.  This allows us to easily translate signal delivery to
    events on condition variables.
    
    Signed-off-by: Dan Cross <cross@gajendra.net>

--- README.md
+++ README.md
@@ -1,7 +1,40 @@
-# Self-Pipe Trick Demonstration
+# Demonstrate Using POSIX Signal Handlers With Thread Condition Variables
 
-This program demonstrates the "self-pipe trick"
-(https://cr.yp.to/docs/selfpipe.html) for safely handling signal
-delivery.  In particular, we show how this technique can be used
-to safely reflect signal delivery into a broadcast on a
-condition variable (which is not permitted in a signal handler).
+These programs demonstrate how to safely handle POSIX signals,
+translating asynchronous signal delivery into events on POSIX
+threads, specifically broadcasting on a condition variables.
+
+## Self-Pipe Trick Demonstration: selfpipe.c
+
+The "self-pipe trick" (https://cr.yp.to/docs/selfpipe.html)
+writes to a "self-pipe" (that is, a pipe that is not shared
+between processes in the usual way) in the signal handler, which
+has the effect of turing asynchronous signal delivery into an IO
+operation.
+
+In our demonstration, we set up threads for each signal of
+itnreest that block on reads from a per-signal pipe, and then
+`pthread_cond_broadcast` on a per-signal condition variable
+whenever those reads return.
+
+## Synchronous sigsetjmp/siglongjmp for signal handlers
+
+Demonstrates a technique where we direct all signals to a single
+thread for handling.  On delivery, the signal handler for any
+given signal will run on that thread, and we use siglongjmp to
+transfer control to the top of the signal handling loop, where
+it can switch and dispatch based on the received signal number.
+
+Because all signals are handled on a single thread, and we know
+that thread does not run non-async-signal-safe code except when
+signals are blocked, we know that we can safely signal a
+condition variable in this context.
+
+## Demonstrate using sigwait and threads
+
+This program demonstrates how to use POSIX `sigwait` to reflect
+asynchronous signal delivery into a return from a blocking
+synchronous call.  Again, we direct all signals to a single
+thread that blocks signals.  When `sigwait` indicates a signal
+was pending for the thread, we `pthread_cond_broadcast` on a
+per-signal condition variable.
---
+++ syncsigwait.c
@@ -0,0 +1,117 @@
+/*
+ * Example of using sigwait with pthreads to broadcast on a
+ * condition variable in response to receiving a signal.
+ *
+ * Dan Cross <net!gajendra!cross>
+ */
+
+#include <assert.h>
+#include <pthread.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+typedef struct Aux Aux;
+struct Aux {
+	const int signum;
+	const char *name;
+	pthread_cond_t cv;
+	pthread_t tid;
+};
+
+static Aux sigx[] = {
+    { SIGINT, "SIGINT", PTHREAD_COND_INITIALIZER },
+    { SIGQUIT, "SIGQUIT", PTHREAD_COND_INITIALIZER },
+    { SIGHUP, "SIGHUP", PTHREAD_COND_INITIALIZER },
+    { SIGTSTP, "SIGTSTP", PTHREAD_COND_INITIALIZER },
+    { SIGUSR1, "SIGUSR1", PTHREAD_COND_INITIALIZER },
+};
+static const size_t NSIGX = sizeof(sigx) / sizeof(sigx[0]);
+
+static pthread_cond_t misccv = PTHREAD_COND_INITIALIZER;
+
+static jmp_buf sigloop;
+
+static void
+notify1(int signum)
+{
+	if (signum != 0) {
+		for (size_t k = 0; k < NSIGX; ++k) {
+			Aux *ap = &sigx[k];
+			if (ap->signum == signum) {
+				pthread_cond_broadcast(&ap->cv);
+				return;
+			}
+		}
+		pthread_cond_broadcast(&misccv);
+	}
+}
+
+static void *
+notifier(void *arg)
+{
+	sigset_t set;
+	(void)arg;
+	int sig;
+
+	sigfillset(&set);
+	pthread_sigmask(SIG_BLOCK, &set, NULL);
+	for (;;) {
+		if (sigwait(&set, &sig) < 0) {
+			perror("sigwait");
+			_exit(EXIT_FAILURE);
+		}
+		notify1(sig);
+	}
+
+	return NULL;
+}
+
+void *
+cvwaiter(void *arg)
+{
+	int times = 0;
+	pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+	Aux *aux = arg;
+
+	assert(aux != NULL);
+
+	pthread_mutex_lock(&mtx);
+	for (;;) {
+		pthread_cond_wait(&aux->cv, &mtx);
+		printf("Recevied signal %s (%d)\n", aux->name, aux->signum);
+		if (++times == 10) {
+			printf("received %s %d times; bailing.\n", aux->name, times);
+			_exit(EXIT_SUCCESS);
+		}
+	}
+}
+
+void
+initsigs(void)
+{
+	for (size_t k = 0; k < NSIGX; ++k) {
+		Aux *aux = &sigx[k];
+		if (pthread_create(&aux->tid, NULL, cvwaiter, aux) < 0) {
+			perror("pthread_create");
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+
+int
+main(void)
+{
+	sigset_t all, set;
+
+	sigfillset(&all);
+	pthread_sigmask(SIG_BLOCK, &all, &set);
+	initsigs();
+	notifier(NULL);
+
+	return 0;
+}
